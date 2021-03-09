630483feb236f7926f5206cffc61453385952e73
unknown <abhishek.ma@nichi.com>
2021-03-09 17:50:28 +0530

    implemnted Sorting of animals based on Name and Weight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngSort';
+  animals = [{ "name": "Goat", "weight": 40 }, { "name": "Tiger", "weight": 70 }, { "name": "Rabbit", "weight": 5 }, { "name": "Buffalo", "weight": 150 }, { "name": "Sheep", "weight": 35 }, { "name": "Elephant", "weight": 400 }];
+
+  nameSort(names) {
+    this.animals.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  weightSort(weights) {
+    this.animals = _.sortBy(this.animals, 'weight');
+
+  }
+
 }
+
