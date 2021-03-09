ac34bc8f11a21ff395ea8ab7d57e9b53343f159d
unknown <abhishek.ma@nichi.com>
2021-03-09 18:00:09 +0530

    comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import * as _ from 'lodash';
+import * as _ from 'lodash';  // Importing lodash for Array operations
 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({   // Decorator 
+  selector: 'app-root',       // selector 
+  templateUrl: './app.component.html',  // Link path of respective view(template) from component
+  styleUrls: ['./app.component.css']     // Link path of respective css files for the view from component
 })
 export class AppComponent {
-  animals = [{ "name": "Goat", "weight": 40 }, { "name": "Tiger", "weight": 70 }, { "name": "Rabbit", "weight": 5 }, { "name": "Buffalo", "weight": 150 }, { "name": "Sheep", "weight": 35 }, { "name": "Elephant", "weight": 400 }];
 
-  nameSort(names) {
+  animals = [ { "name": "Goat", "weight": 40 },  // Delcared Array of Animals with name and weight properties
+              { "name": "Tiger", "weight": 70 }, 
+              { "name": "Rabbit", "weight": 5 }, 
+              { "name": "Buffalo", "weight": 150}, 
+              { "name": "Sheep", "weight": 35}, 
+              { "name": "Elephant", "weight": 400}
+            ];
+
+  nameSort(names) {  // Sorting based on the Animal's Names
     this.animals.sort((a, b) => a.name.localeCompare(b.name));
   }
 
-  weightSort(weights) {
+  weightSort(weights) {  // Sorting based on the Animal's Weights
     this.animals = _.sortBy(this.animals, 'weight');
 
   }
